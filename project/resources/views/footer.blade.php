d2fbef7a006a724358d41285dbb0ef59bc489155
luumyhang <85682767+luumyhang@users.noreply.github.com>
2022-07-23 15:59:55 +0700

    Delete project directory

--- project/resources/views/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<script src="/template/vendor/jquery/jquery-3.2.1.min.js"></script>
-
-<script src="/template/vendor/animsition/js/animsition.min.js"></script>
-
-<script src="/template/js/main.js"></script>
-
-<script src="/template/js/public.js"></script>
